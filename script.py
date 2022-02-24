40b9e0ed50431de99fc1cfed94875467b331a57d
EyznerI <m1702694@edu.misis.ru>
2022-02-24 12:54:32 +0300

    first programm on python

---
+++ script.py
@@ -0,0 +1,14 @@
+print("Hello world")
+print((2 + 3 )* 24 - 17 ** 2 / 14 //15 + 17  - 9)
+
+x = 10 
+y = 5
+my_full_name = "Michel"
+print (x*my_full_name)
+user_name = input("please enter your name:")
+print("Hello," + user_name)
+user_birth = input("please enter your birh year: ")
+user_age = 2022 - int(user_birth)
+print (user_age)
+print(user_name + "," + " your age is " + str(user_age))
+print(f"{user_name}, your age is {user_age}")

22c4c337e9ec4575d6300dfb5bc74b66a44db542
EyznerI <m1702694@edu.misis.ru>
2022-03-03 14:53:19 +0300

    choice products

--- ms.py
+++ ms.py
@@ -1,8 +1,17 @@
 
-user_name = input("please enter your first name:")
-print("Hello," + user_name)
-user_last_name = input("please enter your last name:")
-print("super," + user_name + user_last_name )
-user_MS_programm_start_year = input( "please enter your MS programm start year: ")
-user_MS_programm_finish_year = 2 + int(user_MS_programm_start_year)
-print( + user_MS_programm_finish_year)   
+# user_name = input("please enter your first name:")
+# print("Hello," + user_name)
+# user_last_name = input("please enter your last name:")
+# print("super," + user_name + user_last_name )
+# user_MS_programm_start_year = input( "please enter your MS programm start year: ")
+# user_MS_programm_finish_year = 2 + int(user_MS_programm_start_year)
+# print( + user_MS_programm_finish_year)   
+choice = input("Do you wanna choose coffe or cigarets? please enter: if coffe = 1, if  cigarets = 0 ")
+if choice == "0":
+    age = input("enter your age:")
+    if age > "18":
+        print("ok, take your cigarets")
+    else:
+     print("please, get out of here and come back, when your age will be more 18") 
+else:
+    print("please, take your coffe")

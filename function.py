5510566fc411d770136a42487811abec412da6a4
EyznerI <m1702694@edu.misis.ru>
2022-03-10 18:49:52 +0300

    infinity loop gor shop
    infinity loop gor shop

--- function.py
+++ function.py
@@ -9,17 +9,41 @@
 #     else:
 #         print("you are not adult")
 # age()
-def sum_two(a,b):
-    return a+b
-c = sum_two(5,6)
-print(c)
+# def sum_two(a,b):
+#     return a+b
+# c = sum_two(5,6)
+# print(c)
 
 
-loop = [15,17,19, 41]
-def sum_loop(loop):
-    summ = 0
-    for m in loop:
-        summ = summ+m  
-    return summ
-m = sum_loop(loop)
-print(m)    
+# loop = [15,17,19, 41]
+# def sum_loop(loop):
+#     summ = 0
+#     for m in loop:
+#         summ = summ+m  
+#     return summ
+# m = sum_loop(loop)
+# print(m)    
+def age():
+    b = input("enter your age: ")
+    if int(b) >= 18:
+        print("ok, please take your cigarets")
+    else:
+        print("please, get out of here")
+
+
+while True:
+    choice = input("Do you wanna choose coffe or cigarets? please enter: if coffe = 1, if  cigarets = 0 ")
+    if choice == "0":
+        age()
+    elif choice == "1":
+        print("please, take your coffe")
+    else:
+        continue
+    term = input("terminate (t) or continue(c)? ")
+    if term == "t":
+        break
+    elif term == "c":
+        print("terminate (t) or continue(c)? ")
+    else:
+        continue 
+        
--- optimization 5-ka.py
+++ optimization 5-ka.py
@@ -1,15 +1,22 @@
+def age():
+    b = input("enter your age: ")
+    if int(b) >= 18:
+        print("ok, please take your cigarets")
+    else:
+        print("please, get out of here")
+
+
 while True:
     choice = input("Do you wanna choose coffe or cigarets? please enter: if coffe = 1, if  cigarets = 0 ")
     if choice == "0":
-        age = input("enter your age:")
-        if age >= "18":
-           print("ok, take your cigarets")
-        else:
-           print("please, get out of here and come back, when your age will be more 18") 
-    else:
+        age()
+    elif choice == "1":
         print("please, take your coffe")
+    else:
+        continue
     term = input("terminate (t) or continue(c)? ")
     if term == "t":
         break
     else:
-        continue
+        continue 
+        

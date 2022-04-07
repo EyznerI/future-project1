3aa47853ee755a4a1428f4cb8bcfc7b33b90f5b2
EyznerI <m1702694@edu.misis.ru>
2022-04-07 11:26:08 +0300

    Create what.py

---
+++ what.py
@@ -0,0 +1,45 @@
+# points = []
+# lines = ["Petya", 12, 43, 0]
+# print(len(lines))
+# lines.append("Kolya")
+
+# print(lines[0])
+
+# dict = {"name": "Anna",  "age": 32, "wifes": ["katya", "natasha"]}
+# print(dict["wifes"])
+# dict["status"] = "single"
+# dict['name'] = 'yura'
+# print(dict)
+
+# for k, v in dict.items():
+#     print(k, v)
+
+# customers = []
+# for t in range(3):
+#     user = input("Enter name, age. product: ").split(",")
+#     customers.append({"name": user[0], "age": user[1], "product": user[2] }) 
+
+# total_age = 0
+# for i  in customers:
+#     total_age = total_age + int(i["age"])
+
+# avg_age = total_age / len(customers)
+# print(avg_age)
+
+# for i  in customers:
+#     print(i["age"])
+
+
+# print(customers)
+
+def id ():
+    import random
+    hash = random.getrandbits(64)
+
+    print("hash value: %08x" % hash)
+
+customers = [{"id": "dv0re2f1" }]
+while True
+id costumers = input("hello, Please enter your id: ")
+if   
+
